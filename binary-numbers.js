fd5b322f66ebb53a1dac6b61824a85ebc8221db0
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-05-10 19:28:51 +0300

    Refactoring

--- binary-numbers.js
+++ binary-numbers.js
@@ -1,46 +1,9 @@
 'use strict';
 
-/* function main() {
-    const n = parseInt(readLine(), 10);
-} */
-
-function main() {
-    const n = parseInt(readLine(), 10);
+function main(n) {
     let binaryNumber = n.toString(2);
-    let binaryNumberToArray = binaryNumber.split("").map(num => +num);
-    let groupedArray = regroup(binaryNumberToArray);
-    let maxSum = findMaxSum(groupedArray);
-    console.log(maxSum);
+    let binaryNumberArray = binaryNumber.split("0").map(element => element.length);
+    console.log(Math.max(...binaryNumberArray));
 }
 
 main(158);
-
-function regroup(binaryNumberToArray) {
-    let megaArray = [];
-    let arr = [];
-    for (let i = 0; i < binaryNumberToArray.length; i++) {
-        const currentElement = binaryNumberToArray[i];
-        if (currentElement === 1) {
-            arr.push(currentElement);
-        } else {
-            megaArray.push(arr);
-            arr = [];
-        }
-
-
-    }
-    megaArray.push(arr);
-    return megaArray;
-}
-
-function findMaxSum(groupedArray) {
-    let arrayOfSums = [];
-    for (let element of groupedArray) {
-        let sum = element.reduce(function (a, b) {
-            return a + b;
-        }, 0);
-        arrayOfSums.push(sum);
-    }
-
-    return Math.max(...arrayOfSums);
-}
